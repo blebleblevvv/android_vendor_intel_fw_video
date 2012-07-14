9321dd3343c1586a13ffad542066ecffa19e0368
Bruce Beare <bruce.j.beare@intel.com>
2012-07-14 00:03:11 -0700

    PRODUCT_COPY_FILES: Disable incorrect use
    BZ: 46719
    
    This makfile variable is only able to be set in "product.mk" files -- never
    in Android.mk or AndroidBoard.mk files.
    
    Change-Id: If1bce6a945a43044bc97f4ec9f304d77d9f59143

--- Android.mk
+++ Android.mk
@@ -1,15 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw.bin:system/etc/firmware/msvdx_fw.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw_mfld.bin:system/etc/firmware/msvdx_fw_mfld.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw_mfld_DE2.0.bin:system/etc/firmware/msvdx_fw_mfld_DE2.0.bin
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/lnc/video_fw/topaz_fw.bin:system/etc/firmware/topaz_fw.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/lnc/video_fw/topazsc_fw.bin:system/etc/firmware/topazsc_fw.bin
-
+$(info WARNING: PRODUCT_COPY_FILES disabled in $(LOCAL_PATH))
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw.bin:system/etc/firmware/msvdx_fw.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw_mfld.bin:system/etc/firmware/msvdx_fw_mfld.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw_mfld_DE2.0.bin:system/etc/firmware/msvdx_fw_mfld_DE2.0.bin
+#
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/lnc/video_fw/topaz_fw.bin:system/etc/firmware/topaz_fw.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/lnc/video_fw/topazsc_fw.bin:system/etc/firmware/topazsc_fw.bin
