249f33e8c38df887dc28f89cc062af10d00e3f44
Zhao Liang <leo.zhao@intel.com>
2012-07-19 12:25:58 +0800

    Enable video firmware copying in JellyBean build
    BZ: 47464
    
    enable video firmware copying in JellyBean build
    
    Change-Id: I1aa94b8763be7685201c08fe0229f9cb64aece43
    Signed-off-by: Zhao Liang <leo.zhao@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,15 +1,47 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
-$(info WARNING: PRODUCT_COPY_FILES disabled in $(LOCAL_PATH))
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw.bin:system/etc/firmware/msvdx_fw.bin
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw_mfld.bin:system/etc/firmware/msvdx_fw_mfld.bin
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/lnc/video_fw/msvdx_fw_mfld_DE2.0.bin:system/etc/firmware/msvdx_fw_mfld_DE2.0.bin
-#
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/lnc/video_fw/topaz_fw.bin:system/etc/firmware/topaz_fw.bin
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/lnc/video_fw/topazsc_fw.bin:system/etc/firmware/topazsc_fw.bin
+include $(CLEAR_VARS)
+LOCAL_MODULE := msvdx_fw.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/msvdx_fw.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := msvdx_fw_mfld.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/msvdx_fw_mfld.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := msvdx_fw_mfld_DE2.0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/msvdx_fw_mfld_DE2.0.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := topaz_fw.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/topaz_fw.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := topazsc_fw.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/topazsc_fw.bin
+include $(BUILD_PREBUILT)
+
