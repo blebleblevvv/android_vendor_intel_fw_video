38e81c643ae9af879b0b9944cad3a288fbf219d6
Wang Kun <kun.k.wang@intel.com>
2012-08-30 15:42:24 +0800

    Add the VIED SLE firmware [MRFLD-VIDEO].
    BZ: 54741
    
    Add the SLE firmware
    The VSP firmware only be enabled for mrfl
    
    Change-Id: Iceba729823dd8e639585f32b138066b216a52e00
    Signed-off-by: Wang Kun <kun.k.wang@intel.com>

--- Android.mk
+++ Android.mk
@@ -18,6 +18,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/topazsc_fw.bin
 include $(BUILD_PREBUILT)
 
+ifeq ($(TARGET_PRODUCT),mrfl_vp)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vsp_VPP.bin
 LOCAL_MODULE_OWNER := intel
@@ -26,4 +27,16 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP.bin
 include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(TARGET_PRODUCT),mrfl_sle)
+include $(CLEAR_VARS)
+LOCAL_MODULE := vsp_VPP_sle.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP_sle.bin
+include $(BUILD_PREBUILT)
+endif
 
