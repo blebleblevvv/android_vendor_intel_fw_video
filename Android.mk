872fda4a0deaf93c3e8277c06ec27cbf152a7794
Binglin Chen <binglin.chen@intel.com>
2012-12-11 16:15:19 +0800

    [PORT FROM MAIN][MRFL-Video] add/update vsp/ved/vec firmware
    BZ: 73892
    
    TOPAZHP need a completely new firmware, and MSVDX FW is also updated.
    This patch modificed the Android.mk to update the firmware correctly
    during compiling phase.
    
    Change-Id: I493a0bbe7ab15a774161074cd64496781d5ed420
    Signed-off-by: Binglin Chen <binglin.chen@intel.com>

--- Android.mk
+++ Android.mk
@@ -9,6 +9,16 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/msvdx_fw_mfld_DE2.0.bin
 include $(BUILD_PREBUILT)
 
+ifeq ($(TARGET_PRODUCT), merr_vv)
+include $(CLEAR_VARS)
+LOCAL_MODULE := topazhp_fw.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/topazhp_fw.bin
+include $(BUILD_PREBUILT)
+else
 include $(CLEAR_VARS)
 LOCAL_MODULE := topazsc_fw.bin
 LOCAL_MODULE_OWNER := intel
@@ -17,6 +27,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/topazsc_fw.bin
 include $(BUILD_PREBUILT)
+endif
 
 ifeq ($(REF_PRODUCT_NAME),mrfl_vp)
 include $(CLEAR_VARS)
@@ -27,9 +38,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP.bin
 include $(BUILD_PREBUILT)
-endif
-
-ifeq ($(REF_PRODUCT_NAME),mrfl_sle)
+else ifneq ($(findstring $(TARGET_PRODUCT),merr_vv mrfl_sle),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vsp_VPP_sle.bin
 LOCAL_MODULE_OWNER := intel
@@ -39,4 +48,3 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP_sle.bin
 include $(BUILD_PREBUILT)
 endif
-
