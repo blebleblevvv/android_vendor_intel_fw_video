626a3ec2caddb38b951243d8b9d0289ddc94827b
Sun, Mingruo <mingruo.sun@intel.com>
2013-03-06 19:07:11 +0800

    Upload VSP VP8 firmware to VIED version 1.5 [Video-MRFLD]
    BZ: 90819
    
    The firmware is released by VIED
    
    Change-Id: I15406c702fcc6ebae369589c62f4048f660ae96d
    Signed-off-by: Sun, Mingruo <mingruo.sun@intel.com>

--- Android.mk
+++ Android.mk
@@ -44,3 +44,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP_sle.bin
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vsp_vp8_enc.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/vsp_vp8_enc.bin
+include $(BUILD_PREBUILT)
