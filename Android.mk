a3cdbe18e4f07293a2a305aebf105825d59e8598
Sun, Mingruo <mingruo.sun@intel.com>
2013-07-17 23:01:18 +0800

    update vsp vp8 enc and FRC/VPP firmware to VIED 2.3 release
    BZ: 122096 120615 85193 105680 112608 112235 120859 94737 95889 103601
    
    VP8 encoder and FRC/VPP use the same firmware
    It is released by VIED
    
    Change-Id: I6405bc265c9a64365e53875b106d7f95f3703746
    Signed-off-by: Sun, Mingruo <mingruo.sun@intel.com>

--- Android.mk
+++ Android.mk
@@ -28,28 +28,10 @@ LOCAL_SRC_FILES := lnc/video_fw/topazsc_fw.bin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := vsp_VPP.bin
+LOCAL_MODULE := vsp_vpp_vp8.bin
 LOCAL_MODULE_OWNER := intel
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP.bin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := vsp_VPP_sle.bin
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := lnc/video_fw/vsp_VPP_sle.bin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := vsp_vp8_enc.bin
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := lnc/video_fw/vsp_vp8_enc.bin
+LOCAL_SRC_FILES := lnc/video_fw/vsp_vpp_vp8.bin
 include $(BUILD_PREBUILT)
