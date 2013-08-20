802044105d21edbadbcf0cb26c0bdc5531ed782a
Edward Lin <edward.lin@intel.com>
2013-08-20 05:53:15 +0000

    Implement Video Encoder Secure FW Uploading for Merrifield B0
    BZ: 131895
    
    update the firmware chaabi signed.
    
    Change-Id: Ic223f63946848a57e9bc6f0160faba7739acc9f5
    Signed-off-by: Edward Lin <edward.lin@intel.com>

--- Android.mk
+++ Android.mk
@@ -10,6 +10,15 @@ LOCAL_SRC_FILES := lnc/video_fw/msvdx_fw_mfld_DE2.0.bin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE :=signed_msvdx_fw_mrfld_b0v1.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/signed_msvdx_fw_mrfld_b0v1.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := topazhp_fw.bin
 LOCAL_MODULE_OWNER := intel
 LOCAL_MODULE_TAGS := optional
@@ -17,6 +26,16 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/topazhp_fw.bin
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := topazhp_fw_b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/topazhp_fw_b0.bin
+include $(BUILD_PREBUILT)
+
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := topazsc_fw.bin
@@ -34,4 +53,13 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := lnc/video_fw/vsp_vpp_vp8.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vsp_vpp_vp8_b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/vsp_vpp_vp8_b0.bin
 include $(BUILD_PREBUILT)
