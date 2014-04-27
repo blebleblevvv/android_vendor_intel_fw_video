30ea8547b94c53c85c80811597052c5af35971f9
Fei Jiang <fei.jiang@intel.com>
2014-04-27 23:50:24 +0800

    msvdx firmwares add back
    BZ: 190593
    
    Baytrail still need msvdx_fw_mfld_DE2.0.bin,
    otherwise VP8/MEPG4/H263 will fail to playback.
    
    Change-Id: Ic02735af05f74c99e81f8e42a7a8e8c0add112ef
    Signed-off-by: Fei Jiang <fei.jiang@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := msvdx_fw_mfld_DE2.0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := lnc/video_fw/msvdx_fw_mfld_DE2.0.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := topazsc_fw.bin
 LOCAL_MODULE_OWNER := intel
 LOCAL_MODULE_TAGS := optional
